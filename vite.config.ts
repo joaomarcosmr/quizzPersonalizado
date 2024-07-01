d3ac483eba5bf26a80e87e4d4dbd0a19cca1b404
Felipe Miranda <joaomarcosmr4@gmail.com>
2024-07-01 13:49:42 -0300

    chang

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quizzPersonalizado/', // Adicione a base URL do seu repositório
   plugins: [react()],
-	base: "/quizzPersonalizado"
-})
+});
